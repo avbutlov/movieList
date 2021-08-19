60c3821a5990f81c5ef41ead78dc1a107b85cf3c
Aleksandr Butlov <butloff666@gmail.com>
2021-08-19 17:53:01 +0300

    add redirection

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,18 +2,18 @@ import React from "react";
 import "./App.css";
 import ColumnsContainer from "../ColumnsContainer/ColumnsContainer";
 import Header from "../Header/Header";
-import {Switch, Route } from "react-router-dom";
+import { Route, Redirect } from "react-router-dom";
 import HomePage from "../HomePage/HomePage";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Switch>
-      <Route path='/movies' component={ColumnsContainer}/>
-      <Route path='/home' component={HomePage}/>
-      <Route path='/' component={HomePage}/>
-      </Switch>
+      <Route path="/movies" component={ColumnsContainer} />
+      <Route path="/home" component={HomePage} />
+      <Route exact path="/">
+        <Redirect to="/home" />
+      </Route>
     </div>
   );
 }
